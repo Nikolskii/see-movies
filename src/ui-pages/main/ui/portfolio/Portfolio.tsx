9f60c4dc8cea1bfe8b14491da1bddbb53239374c
Denis Nikolskii <denis-nikolskii@ya.ru>
2025-12-18 22:29:17 +0200

    feat: добавить компонент portfolio

--- src/ui-pages/main/ui/portfolio/Portfolio.tsx
+++ src/ui-pages/main/ui/portfolio/Portfolio.tsx
@@ -1,7 +1,36 @@
+const LINKS = [
+  {
+    title: 'Статичный сайт',
+    href: 'https://github.com/Nikolskii/how-to-learn',
+  },
+  {
+    title: 'Адаптивный сайт',
+    href: 'https://github.com/Nikolskii/russian-travel',
+  },
+  {
+    title: 'Одностраничное приложение',
+    href: 'https://github.com/Nikolskii/react-mesto-api-full',
+  },
+];
+
 export function Portfolio() {
   return (
     <section className="px-3.5 py-[70px] md:px-[50px] md:py-[100px] lg:px-[70px]">
-      <h3>Портфолио</h3>
+      <h3 className="text-text-muted mb-3 text-[14px] md:text-[18px]">Портфолио</h3>
+      <ul>
+        {LINKS.map(({ title, href }) => (
+          <li className="not-last:border-light-700 not-last:border-b" key={href}>
+            <a
+              className="action-fade block bg-[url('/arrow.svg')] bg-size-[12px_12px] bg-right bg-no-repeat py-5 pr-2.5 text-[18px] md:bg-size-[16px_16px] md:py-6 md:text-[28px] lg:text-[30px]"
+              href={href}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
