ef955f013fbd66d3e3b25ebb1cb1a2ab0b3ed0fe
Denis Nikolskii <denis-nikolskii@ya.ru>
2025-12-27 22:29:12 +0200

    feat: добавить компонент AuthButtons для управления авторизацией пользователя

---
+++ src/features/auth/ui/AuthButtons.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export function AuthButtons() {
+  return (
+    <div className="flex items-center gap-3.5 md:gap-[30px]">
+      <Link className="action-fade text-[10px] font-medium md:text-[12px]" href="/signup">
+        Регистрация
+      </Link>
+      <Link
+        className="action-fade rounded-[3px] bg-green-500 px-3 py-[5px] text-[10px] text-black md:px-5 md:py-2 md:text-[12px]"
+        href="/signin"
+      >
+        Войти
+      </Link>
+    </div>
+  );
+}
