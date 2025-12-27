1dcf35659d94b6e35c3097657335754518e4f70d
Denis Nikolskii <denis-nikolskii@ya.ru>
2025-12-27 22:29:40 +0200

    chore: обновить компонент Header для улучшения управления авторизацией и добавления меню

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,17 +1,43 @@
+'use client';
+
+import { Menu } from 'lucide-react';
+import { useState } from 'react';
+
 import { useUser } from '@/entities/user';
+import { AuthButtons } from '@/features/auth';
 import { ProfileLink } from '@/features/profile';
-import { Logo } from '@/shared/ui';
+import { Drawer, Logo, ThemeToggle } from '@/shared/ui';
 
 export function Header() {
   const { isAuthorized } = useUser();
-
-  console.log('isAuthorized', isAuthorized);
+  // TODO: как назвать эту переменную?
+  const [isMenuDrawerOpen, setIsMenuDrawerOpen] = useState(false);
 
   return (
-    <header className="flex justify-between">
-      <Logo />
-      {/* <div>{navigation}</div> */}
-      {isAuthorized ? <ProfileLink /> : <button>Войти</button>}
-    </header>
+    <>
+      <header className="flex justify-between">
+        <Logo />
+        <ThemeToggle />
+        {/* <div>{navigation}</div> */}
+        {isAuthorized ? (
+          <div className="hidden lg:inline">
+            <ProfileLink />
+          </div>
+        ) : (
+          <AuthButtons />
+        )}
+        {isAuthorized ? (
+          <button
+            className="action-fade cursor-pointer lg:hidden"
+            onClick={() => setIsMenuDrawerOpen(true)}
+          >
+            <Menu size={30} />
+          </button>
+        ) : null}
+      </header>
+      <Drawer open={isMenuDrawerOpen} onClose={() => setIsMenuDrawerOpen(false)}>
+        <div>Контент drawer</div>
+      </Drawer>
+    </>
   );
 }
