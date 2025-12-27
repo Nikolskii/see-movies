29294d7faeed937c2b707371db0aab2ac8896aa6
Denis Nikolskii <denis-nikolskii@ya.ru>
2025-12-27 22:28:13 +0200

    chore: удалить компонент ThemeToggle из MainPage

--- src/ui-pages/main/MainPage.tsx
+++ src/ui-pages/main/MainPage.tsx
@@ -1,17 +1,18 @@
-import { Footer, ThemeToggle } from '@/shared/ui';
+import { Footer } from '@/shared/ui';
 import { AboutMe, AboutProject, Portfolio, Promo, Techs } from '@/ui-pages/main/ui';
+import { Header } from '@/widgets';
 
 export function MainPage() {
   return (
     <div className="dark:bg-dark-900 bg-color-light-900 min-h-screen text-black dark:text-white">
       <div className="container mx-auto">
+        <Header />
         <Promo />
         <AboutProject />
         <Techs />
         <AboutMe />
         <Portfolio />
         <Footer />
-        <ThemeToggle />
       </div>
     </div>
   );
