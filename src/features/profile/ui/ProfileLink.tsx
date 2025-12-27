0949454e0233871517644fc2b2aedd57bcf60dd1
Denis Nikolskii <denis-nikolskii@ya.ru>
2025-12-27 22:28:28 +0200

    fix: добавить недостающий класс font-medium в компонент ProfileLink

--- src/features/profile/ui/ProfileLink.tsx
+++ src/features/profile/ui/ProfileLink.tsx
@@ -4,7 +4,7 @@ export function ProfileLink() {
   return (
     <Link
       href="/profile"
-      className="dark:bg-dark-500 bg-light-500 action-fade rounded-[20px] px-5 py-2 dark:text-white"
+      className="dark:bg-dark-500 bg-light-500 action-fade rounded-[20px] px-5 py-2 text-[14px] font-medium dark:text-white"
     >
       Аккаунт
     </Link>
