0789e55378276a64f23f98a9064302be8eb41bca
Denis Nikolsky <den-apple@ya.ru>
2026-01-01 21:55:28 +0200

    feat: обновить компонент MobileMenu для использования динамических навигационных ссылок

--- src/widgets/header/ui/MobileMenu.tsx
+++ src/widgets/header/ui/MobileMenu.tsx
@@ -2,28 +2,26 @@
 
 import clsx from 'clsx';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 import { ProfileLink } from '@/features/profile';
 import { isRouteActive } from '@/shared/lib';
 import { routes } from '@/shared/routes';
+import { navLinks } from '@/widgets/header/model/nav';
 
-const LINKS = [
-  { href: routes.home, label: 'Главная' },
-  { href: routes.movies, label: 'Фильмы' },
-  { href: routes.savedMovies, label: 'Сохранённые фильмы' },
-];
+const menuLinks = [{ href: routes.home, label: 'Главная' }, ...navLinks];
 
 // TODO актуализировать стили активного пункта меню
 
-export function MobileMenu() {
-  const pathname = usePathname();
+type Props = {
+  pathname: string;
+};
 
+export function MobileMenu({ pathname }: Props) {
   return (
     <div className="flex min-h-screen flex-col justify-between gap-10 px-2.5 pt-36 pb-[46px] md:pt-40 md:pb-[100px]">
       <nav>
         <ul className="flex flex-col gap-4 text-center">
-          {LINKS.map(({ href, label }) => {
+          {menuLinks.map(({ href, label }) => {
             const isActive = isRouteActive({ href, pathname });
 
             return (
