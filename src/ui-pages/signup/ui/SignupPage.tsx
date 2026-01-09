b161d7d63fcf5577e2b14cea18a9dd4505e4b0b3
Denis Nikolskii <denis-nikolskii@ya.ru>
2026-01-09 21:35:13 +0200

    feat: улучшить структуру SignupPage, добавив flexbox для контейнера и основного содержимого

--- src/ui-pages/signup/ui/SignupPage.tsx
+++ src/ui-pages/signup/ui/SignupPage.tsx
@@ -4,13 +4,12 @@ import { SignupForm } from '@/features/auth';
 import { routes } from '@/shared/routes';
 import { Logo, ThemeToggle } from '@/shared/ui';
 
-// TODO: добавить переключатель темы
 // TODO: вынести стили в общий app layout
 
 export function SignupPage() {
   return (
     <div className="dark:bg-dark-900 bg-color-light-900 min-h-screen text-black dark:text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex min-h-screen flex-col">
         <header className="flex flex-col items-center justify-center">
           <div className="mb-5 flex w-full justify-end p-3">
             <ThemeToggle />
@@ -20,10 +19,10 @@ export function SignupPage() {
           </div>
           <h1 className="mb-20 text-[24px] font-medium">Добро пожаловать!</h1>
         </header>
-        <main className="px-[30px]">
+        <main className="flex flex-1 flex-col px-[30px] pb-3.5">
           <SignupForm />
         </main>
-        <footer>
+        <footer className="pb-[30px]">
           <p className="text-center text-[12px] text-gray-500 md:text-[14px]">
             Уже зарегистрированы?{' '}
             <Link className="action-fade text-blue-500" href={routes.signin}>
