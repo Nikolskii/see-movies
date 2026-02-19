3f0cbcc05955a96572ff35e9438cad94e39b64cb
Denis Nikolskii <denis-nikolskii@ya.ru>
2026-02-19 21:16:21 +0200

    feat: add formatDuration function to convert movie duration from minutes to formatted string

---
+++ src/entities/movie/lib/formatDuration.ts
@@ -0,0 +1,14 @@
+export function formatDuration(minutes: number) {
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+
+  if (hours === 0) {
+    return `${mins}м`;
+  }
+
+  if (mins === 0) {
+    return `${hours}ч`;
+  }
+
+  return `${hours}ч ${mins}м`;
+}
