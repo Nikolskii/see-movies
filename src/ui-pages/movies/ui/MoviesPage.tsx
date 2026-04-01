9366b60a5a229afcc49c318f21cb9eba963b4889
Denis Nikolskii <denis-nikolskii@ya.ru>
2026-04-01 19:59:26 +0200

    feat: implement SearchMoviesForm component with normalized query handling

--- src/ui-pages/movies/ui/MoviesPage.tsx
+++ src/ui-pages/movies/ui/MoviesPage.tsx
@@ -1,35 +1,32 @@
-import { filterShortMovies, mergeBeatfilmWithSaved } from '@/entities/movie';
+import { filterMovies, mergeBeatfilmWithSaved } from '@/entities/movie';
 import { getBeatfilmMovies, getSavedMovies } from '@/entities/movie/api/server';
 import { ShortMoviesToggle } from '@/features/filter-short-movies';
+import { SearchMoviesForm } from '@/features/search-movies';
 import { Footer } from '@/shared/ui';
 import { MoviesList } from '@/ui-pages/movies/ui/MoviesList';
 import { Header } from '@/widgets/header';
 
 type Props = {
   isShortOnly: boolean;
+  searchQuery: string;
 };
 
-export async function MoviesPage({ isShortOnly }: Props) {
+export async function MoviesPage({ isShortOnly, searchQuery }: Props) {
   const beatfilmMovies = await getBeatfilmMovies();
   const savedMovies = await getSavedMovies();
 
   const mergedMovies = mergeBeatfilmWithSaved({ beatfilmMovies, savedMovies });
-  const filteredMovies = isShortOnly ? filterShortMovies(mergedMovies) : mergedMovies;
-
-  //   const filteredMovies = filterMovies({
-  //   movies: mergedMovies,
-  //   query: searchQuery,
-  //   isShortOnly,
-  // });
-
-  // const visibleMovies = filteredMovies.slice(0, visibleCount);
+  const filteredMovies = filterMovies({ movies: mergedMovies, query: searchQuery, isShortOnly });
 
   return (
     <div className="page-root">
       <div className="container mx-auto flex min-h-screen flex-col">
         <Header isAuthorized />
-        <div className="mb-[40px]">
-          <ShortMoviesToggle />
+        <div className="mb-[40px] px-[14px] pt-[80px] md:px-[30px] lg:px-[70px]">
+          <div className="flex flex-col gap-[45px] border-b border-light-700 pb-[40px] sm:gap-[30px] dark:border-dark-700">
+            <SearchMoviesForm />
+            <ShortMoviesToggle />
+          </div>
         </div>
         <div className="flex-1">
           <MoviesList movies={filteredMovies} />
--- src/ui-pages/saved-movies/ui/SavedMoviesPage.tsx
+++ src/ui-pages/saved-movies/ui/SavedMoviesPage.tsx
@@ -1,26 +1,30 @@
-import { filterShortMovies } from '@/entities/movie';
+import { filterMovies } from '@/entities/movie';
 import { getSavedMovies } from '@/entities/movie/api/server';
 import { ShortMoviesToggle } from '@/features/filter-short-movies';
+import { SearchMoviesForm } from '@/features/search-movies';
 import { Footer } from '@/shared/ui';
 import { SavedMoviesList } from '@/ui-pages/saved-movies/ui/SavedMoviesList';
 import { Header } from '@/widgets/header';
 
 type Props = {
   isShortOnly: boolean;
+  searchQuery: string;
 };
 
-export async function SavedMoviesPage({ isShortOnly }: Props) {
+export async function SavedMoviesPage({ isShortOnly, searchQuery }: Props) {
   const savedMovies = await getSavedMovies();
 
-  const filteredMovies = isShortOnly ? filterShortMovies(savedMovies) : savedMovies;
+  const filteredMovies = filterMovies({ movies: savedMovies, query: searchQuery, isShortOnly });
 
   return (
     <div className="page-root">
       <div className="container mx-auto flex min-h-screen flex-col">
         <Header isAuthorized />
-        {/* Form and input search movie */}
-        <div className="mb-[40px]">
-          <ShortMoviesToggle />
+        <div className="mb-[40px] px-[14px] pt-[80px] md:px-[30px] lg:px-[70px]">
+          <div className="flex flex-col gap-[45px] border-b border-light-700 pb-[40px] sm:gap-[30px] dark:border-dark-700">
+            <SearchMoviesForm />
+            <ShortMoviesToggle />
+          </div>
         </div>
         <div className="flex-1">
           <SavedMoviesList movies={filteredMovies} />
