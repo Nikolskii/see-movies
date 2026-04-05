f0e75e2302b4b83f7bac50b85fbe046bb7069655
Denis Nikolskii <denis-nikolskii@ya.ru>
2026-04-05 09:14:10 +0200

    feat: enhance getBeatfilmMovies with caching and revalidation settings

--- src/entities/movie/api/server/getBeatfilmMovies.ts
+++ src/entities/movie/api/server/getBeatfilmMovies.ts
@@ -4,13 +4,17 @@ import { BeatfilmMovie } from '@/entities/movie/model/types';
 import { apiFetch } from '@/shared/api';
 import { NOMOREPARTIES_API_URL } from '@/shared/config/env';
 
-// TODO: кешировать запрос SSG или ISR
+// TODO: проверить по логам на бэк, что запрос закеширован
+
+const BEATFILM_REVALIDATE_SECONDS = 60 * 60 * 24;
 
 export const getBeatfilmMovies = cache(async () => {
   return apiFetch<BeatfilmMovie[]>(
     '/beatfilm-movies',
     {
       method: 'GET',
+      credentials: 'omit',
+      next: { revalidate: BEATFILM_REVALIDATE_SECONDS },
     },
     NOMOREPARTIES_API_URL
   );
